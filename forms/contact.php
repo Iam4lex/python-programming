5607bc2d3f0d80e1ffae978845366cec966f7ca3
Alex Mwangi <mungaialexm@gmail.com>
2024-05-10 19:41:17 +0300

    Read more page customization

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,4 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
