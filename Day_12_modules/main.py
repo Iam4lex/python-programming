3f0b50fe1100aedb865ba943b65e23508bbc8eba
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-12 12:28:28 +0300

    Initial commit

--- Day_12_modules/main.py
+++ Day_12_modules/main.py
@@ -1,9 +1,14 @@
 # Import the module using the import keyword
 
-import module # import thr module
-
-print(module.my_name("Alex","Mwangi"))
-
 # importing functions from a module
+# - We can import many function which are inside the module
 
-                        
+from module import my_name, total, area_of_circle
+
+
+print(my_name(input("Enter the first name : "), input("Enter the second name : ")))
+
+print(total(first_number = int(input("Enter the first number ")),
+    second_number = int(input("Enter the second number "))))
+
+print(area_of_circle(radius = int(input("Enter the radois of the circle\n: "))))
--- Day_12_modules/module.py
+++ Day_12_modules/module.py
@@ -2,6 +2,23 @@
 
 # Lets create a function inside the module which we can access in the main program
 
+# first function
 def my_name(first_name, second_name): # define a function called my_name
 
+    # first_name = input("Enter the first name : ")
+    # second_name = input("Enter the second name : ")
+
     return first_name, second_name
+
+# second function
+def total(first_number, second_number):
+    
+    sum_of_numbers = first_number + second_number
+
+    return sum_of_numbers
+
+def area_of_circle(radius):
+    
+    area = ( 22 * radius * radius ) / 7
+
+    return area
