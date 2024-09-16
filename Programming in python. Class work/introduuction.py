ea2710420145c39e6fb27a6f1cb4aea565e13258
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-16 09:16:24 +0300

    Initial commit

--- Programming in python. Class work/introduuction.py
+++ Programming in python. Class work/introduuction.py
@@ -3,4 +3,16 @@
 # abs function
 # int function
 # round function - round off numbers round(2.345,2) -> round the number to 2 decimal places
-# 
+# Example
+
+a = 2
+b = 3
+
+print(abs(1 - (4 * b))) # prints 11
+
+# Arguments assignments
+# example
+
+counter = 0
+counter = counter + 1
+counter += 1 # same thing
