97280784bf8142fecd3358c2ace2e3e5c55899e8
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-16 09:34:55 +0300

    Initial commit

--- Programming in python. Class work/introduuction.py
+++ Programming in python. Class work/introduuction.py
@@ -16,3 +16,30 @@ print(abs(1 - (4 * b))) # prints 11
 counter = 0
 counter = counter + 1
 counter += 1 # same thing
+
+
+totoalInches = 41
+feet = totoalInches // 12
+inches = totoalInches % 12
+print(feet, inches)
+
+
+# Syntax errors
+# Run time erros
+
+# print( 5 / 0) # Will produce a runtime error
+
+# Logic erros
+# Numeric objects in memory
+n = 5
+n = 7
+
+print(n)
+
+#    WEEK 2
+# Lists and Tuples
+# example of a list
+# systax => list_name = ['list']
+
+list_of_fluits = ['Mango', 'Apple', "Avocado", "Mango"]
+print("The list of fluits is ", list_of_fluits)
