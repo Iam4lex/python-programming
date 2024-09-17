e91100b94f272f55c75d3be0b0b322433d000bc2
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-17 10:10:03 +0300

    Initial commit

---
+++ Day_13_List_comprehension/List_comprehension.py
@@ -0,0 +1,18 @@
+
+# You can change a string of characters into a list in different ways.
+# Example
+
+my_name = "Alex Mwangi"
+
+change_to_list = list(my_name) # Chnage the string my_name to list
+
+print("My name is ", my_name)
+print("The list is ", change_to_list)
+
+# Another way to convert the list is
+
+full_name = "Alex Mwangi"
+
+the_list = [i for i in full_name]
+
+print("The same list is ",the_list)
