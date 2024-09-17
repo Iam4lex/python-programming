ee8c1d2162d31aae2b84d8766131f743d8d22aa7
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-17 10:25:14 +0300

    Initial commit

--- Day_13_List_comprehension/List_comprehension.py
+++ Day_13_List_comprehension/List_comprehension.py
@@ -16,3 +16,21 @@ full_name = "Alex Mwangi"
 the_list = [i for i in full_name]
 
 print("The same list is ",the_list)
+
+
+list_of_numbers = [i for i in range(10)] # Numbers between 0 and 10 ie. 1-9
+
+print(list_of_numbers) # prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# You can also print the squres of the list_of_numbers
+
+squre_of_numbers = [i * i for i in range(10)]
+
+print(squre_of_numbers)
+
+
+# You can also print list of tuples
+
+list_of_tuples = [(i , i * i) for i in range(10)]
+
+print(list_of_tuples)
