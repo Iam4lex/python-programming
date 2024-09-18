077048a1b5401e578c565efc66391d75a61e981d
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-18 12:40:26 +0300

    Initial commit

---
+++ Day_14_higher_order_functions/Higher_order_functions.py
@@ -0,0 +1,8 @@
+def sum_numbers(nums):  # normal function
+    return sum(nums)    # a sad function abusing the built-in sum function :<
+
+def higher_order_function(f, lst):  # function as a parameter
+    summation = f(lst)
+    return summation
+result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
+print(result)       # 15
---
+++ Programming in python. Class work/largest_number.py
@@ -0,0 +1,14 @@
+
+# A program to find the largest number
+
+first_number  = int(input("Enter the first number "))
+second_number = int(input("Enter the second number "))
+
+if first_number > second_number:
+    print(first_number, " is greater than ", second_number)
+
+elif first_number == second_number:
+    print(first_number, " is equal to ", second_number)
+
+else:
+    print(second_number, " is greater than ", first_number)
