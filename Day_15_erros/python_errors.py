d537bb78f9a93afca5084ae31d696280ab979fe6
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-09-23 21:54:06 +0300

    Initial commit

---
+++ Day_15_erros/python_errors.py
@@ -0,0 +1,12 @@
+
+# Python has alot of types of errors that we can make when writing our code.
+
+# 1. Module not found error
+
+# import maths # This will print an error
+import math # This is correct
+
+# 2. Attriubute error
+math.PI # This will print an error AttributeError: module 'math' has no attribute 'PI'. Did you mean: 'pi'?
+
+
