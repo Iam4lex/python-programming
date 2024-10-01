f30592d3fab2b5b1d7b6c6708ca45b013eeca0d9
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-10-01 21:03:31 +0300

    Modified utility programs: factorial off a number

---
+++ Utility programs/even_odd.py
@@ -0,0 +1,12 @@
+
+# Check if a number is even or odd
+
+number = input("Please enter the first number: ")
+
+number = int(number)
+
+if number % 2 == 0:
+    print("Even number")
+
+else:
+    print("Odd number")
--- Utility programs/factorial.py
+++ Utility programs/factorial.py
@@ -1,10 +1,12 @@
-# Program to calculate the factorial of a number
 
-number = int(input("Enter the number "))
+# Print the factorial of a number
+
+number = input("Please enter the number to check the factorial: ")
+number = int(number)
 
 fact = 1
 
-for i in range(1, number+1) :
+for i in range(1, number+1):
     fact = fact*i
 
-print(fact)
+print(f"The factorial of {number} is {fact}")
