707370f3bd7a5d3a50c832dd70d0a931958828d7
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-10-01 22:49:00 +0300

    Utiliti programs: Fibonachi series

---
+++ Utility programs/fibonachi_series.py
@@ -0,0 +1,15 @@
+
+# Fibonachi is a series of numbers where each number is the sum of the 2 proceeding numbers
+
+a = 0
+b = 1
+
+number_of_times = input("Enter the number of times to generate the series: ")
+number_of_times = int(number_of_times)
+
+print("FIBONACHI SEIRIES")
+for i in range(1,  number_of_times+1):
+    c = a + b
+    a = b
+    b = c
+    print(f" {c}", end="")
