db41a69397729ed5006b715a7730c9df8a1fe18f
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-10-12 18:36:12 +0300

    Day 11: Modules

--- 100 days challange/Day_12_modules/main.py
+++ 100 days challange/Day_12_modules/main.py
@@ -1,15 +1,12 @@
-# Import the module using the import keyword
 
-# importing functions from a module
-# - We can import many function which are inside the module
+import my_module
 
-from module import my_name, total, area_of_circle
+# importing a specific function from a module
+from my_module import area_of_circle 
+
+# Generate the full name
+print(my_module.generate_full_name("Alex", "Mwangi"))
 
 
-print(my_name(input("Enter the first name : "), input("Enter the second name : ")))
-
-print(total(first_number = int(input("Enter the first number ")),
-    second_number = int(input("Enter the second number "))))
-
-print(area_of_circle(radius = int(input("Enter the radois of the circle\n: "))))
-
+# Calculate area of a circle
+print(area_of_circle(7))
---
+++ 100 days challange/Day_12_modules/my_module.py
@@ -0,0 +1,15 @@
+
+# Generate the full name
+
+import math
+
+def generate_full_name(first_name, last_name):
+
+    return first_name + ' ' + last_name
+
+
+# Area of a circle
+
+def area_of_circle(radius):
+
+    return math.pi * radius**2
