9a9a8434f2abbe5095dfa78bc3182e443863ef31
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-11-06 10:28:50 +0300

    Demo

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,14 +1,9 @@
 
+from kivy.app import App
 
+class Demo(App):
 
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel, MDIcon
-
-class Demo(MDApp):
     def build(self):
-        label = MDLabel(text="Hello world", halign="center", theme_text_color="Custom",
-        text_color=(0, 0, 1, 1))
-
-        icon_label = MDIcon(icon="Library-video", halign="center")
-        return icon_label
+        return super().build()
+    
 Demo().run()
