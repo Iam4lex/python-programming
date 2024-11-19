0e425c58af3b495bc6abaf9545a790884a30728c
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-11-19 22:07:00 +0300

    minor changes

---
+++ Generic programming/Exercise/009.py
@@ -0,0 +1,10 @@
+
+hours_worked = 200
+
+if hours_worked > 40:
+    worked_overtime = True
+    print(worked_overtime)
+
+else:
+    worked_overtime = False
+    print(worked_overtime)
--- Generic programming/Exercise/010.py
+++ Generic programming/Exercise/010.py
@@ -1,4 +1,11 @@
+x = 10
+y = 5
+z = 2
 
-name = "kamau"
+smallest = x  # Assume x is the smallest initially
+if y < smallest:
+    smallest = y
+if z < smallest:
+    smallest = z
 
-print(name[:4])
+print(smallest)  # Output will be 2, as z is the smallest
